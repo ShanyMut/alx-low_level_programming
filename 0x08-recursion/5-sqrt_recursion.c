4535d0269b3125f251c64e106f62e35520a5393a
ShanyMut <sharonmutabonwa678@gmail.com>
2024-08-20 12:26:29 +0200

    natural square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * _sqrt_helper - calculates power operation
+ * @n: input number
+ * @guess: iterator
+ */
+
 /*Helper function to perform the recursive square root calculation*/
 int _sqrt_helper(int n, int guess)
 {
 	/*Base case 1: if guess squared is equal to n, return guess*/
 	if (guess * guess == n)
 	{
-		return guess;
+		return (guess);
 	}
 
-	/*Base case 2: if guess squared is greater than n, 
-	 * return -1 (no natural square root)*/
+
 	if (guess * guess > n)
 	{
-		return -1;
+		return (-1);
 	}
 
 	/*Recursive case: increment guess and check again*/
-	return _sqrt_helper(n, guess + 1);
+	return (_sqrt_helper(n, guess + 1));
 }
 
 /*Main function to find the natural square root of n*/
@@ -26,8 +33,8 @@ int _sqrt_recursion(int n)
 	/*If n is negative, it has no natural square root, so return -1*/
 	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
 	/*Start the recursion with an initial guess of 1*/
-	return _sqrt_helper(n, 1);
+	return (_sqrt_helper(n, 1));
 }
